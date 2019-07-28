7d992803e0f1e8882f8b7da80cc3564bdc5a6534
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-07-28 22:16:05 +0300

    decoded token

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../shared/services/auth.service';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {User} from '../shared/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,14 @@ import {AuthService} from '../shared/services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
+  userOwner: User;
+
   constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
     this.router.navigate(['/projects']);
+    this.userOwner = this.auth.decodeToken();
+    console.log('jwt token - ' + this.userOwner.username);
   }
 
   onSubmit() {
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Router} from '@angular/router';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthService {
@@ -10,6 +11,15 @@ export class AuthService {
   private loginUrl = `${environment.url}/auth/login`;
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  jwtHelper = new JwtHelperService();
+
+  decodeToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return this.jwtHelper.decodeToken(token);
+    }
+  }
 
   registrationUser(user) {
      return this.http.post<any>(this.registerUrl, user);
@@ -28,7 +38,4 @@ export class AuthService {
     this.router.navigate(['/login']);
   }
 
-/*  getToken() {
-    return localStorage.getItem('token');
-  }*/
 }
