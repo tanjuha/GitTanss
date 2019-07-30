574472a6fbc1f913d7a2d273a5c63f16cb2c2bc7
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-07-30 04:43:01 +0300

    added messade

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../../shared/services/auth.service';
+import {Message} from '../../shared/models/message.model';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,23 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router, private auth: AuthService) { }
 
   formLogin: FormGroup;
+  message: Message;
 
   ngOnInit() {
+    this.message = new Message('danger', '');
     this.formLogin = new FormGroup({
       'username': new FormControl(null, [Validators.required]),
       'password': new FormControl(null, [Validators.required])
     });
 
   }
+  private showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
   onSubmit() {
     const formData = this.formLogin.value;
     this.auth.loginUser(formData).subscribe(
@@ -35,3 +45,4 @@ export class LoginComponent implements OnInit {
 }
 
 }
+
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import {User} from '../shared/models/user.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
